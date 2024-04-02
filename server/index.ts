f8f5b1b4c4ef7d8d1dd691c860bed5b3c42da622
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2024-04-02 22:26:44 +0530

    setting up routing

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 
 dotenv.config();
@@ -9,13 +10,17 @@ dotenv.config();
 mongoose.connect(process.env.MONGODB_URL, {
     userNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 }).then(()=>{
     console.log("Database connected")
 }).catch((error:Error)=>{
     console.log("databse connection failed....",error)
 })
+app.use(express.json());
+app.use("/server/auth", authRoute);
 
 
-app.listen(8800, () => {
-  console.log("Server is running on port 3000");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
+
---
+++ server/routes/auth.ts
@@ -0,0 +1,25 @@
+import { Router} from "express";
+import User from "../models/User";
+
+
+const router = Router();
+
+// register thie user
+
+router.post("/register",async (req,res)=>{
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    console.log("newUser",newUser)
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("error,,,,",error)
+        res.status(500).json(error);
+    }
+})
+
+module.exports = router;
