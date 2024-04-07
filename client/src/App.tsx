fdab0b347e99c6dbd09ccd84613acb2a542b4305
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2024-04-07 21:55:22 +0530

    making ui with tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odio quisquam voluptatum. Enim pariatur aliquam velit corporis est repellat nihil excepturi, maxime commodi nostrum atque nobis, id repudiandae, ea illo iusto doloribus reiciendis aspernatur neque dolore! Accusantium reprehenderit corporis quae culpa voluptas vel.</div>
+  )
 }
 
-export default App;
+export default App
+
---
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div
+        className="bg-cover bg-center h-screen"
+        style={{ backgroundImage: `url(/netflixLandingBackgound.jpg)` }}
+      ></div>
+    </div>
+  );
+};
+
+export default LandingPage;
